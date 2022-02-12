77c146df7a74427823ee9982672e92203f1bf66b
Lucas Hang <lucas.hhang@gmail.com>
2022-02-12 12:54:29 -0300

    fix: scss imports

--- components/artCard/artCard.tsx
+++ components/artCard/artCard.tsx
@@ -1,6 +1,6 @@
 import { FC } from 'react';
 import Link from 'next/link';
-import styles from './artCard.module.css';
+import styles from './artCard.module.scss';
 import Image, { ImageProps } from 'next/image';
 
 interface ArtCardProps {
