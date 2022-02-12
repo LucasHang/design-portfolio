c4c3c0d85fd3c3ac2a28085b56dac55cb92f693c
Lucas Hang <lucas.hhang@gmail.com>
2022-02-12 12:21:10 -0300

    feat: project initialization

---
+++ components/artCard/artCard.tsx
@@ -0,0 +1,36 @@
+import { FC } from 'react';
+import Link from 'next/link';
+import styles from './artCard.module.css';
+import Image, { ImageProps } from 'next/image';
+
+interface ArtCardProps {
+    art: { name: string; slug: string; description: string; url: string };
+    imgProps?: Omit<ImageProps, 'src' | 'layout' | 'placeholder' | 'blurDataURL'>;
+}
+
+const placeholderImg = '/art-img-placeholder.svg';
+
+const ArtCard: FC<ArtCardProps> = ({ art, imgProps }) => {
+    return (
+        <Link href={`/arts/${art.slug}`}>
+            <a className={styles.slim} aria-label={art.name}>
+                <div className={styles.header}>
+                    <span>{art.name}</span>
+                </div>
+                <div>
+                    <Image
+                        quality="85"
+                        src={art.url || placeholderImg}
+                        alt={art.name || 'Art Image'}
+                        height={320}
+                        width={320}
+                        layout="fixed"
+                        {...imgProps}
+                    />
+                </div>
+            </a>
+        </Link>
+    );
+};
+
+export default ArtCard;
---
+++ prettier.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  singleQuote: true,
+  trailingComma: 'all',
+  arrowParens: 'avoid',
+  printWidth: 112,
+  tabWidth: 4,
+  endOfLine: 'auto',
+};
