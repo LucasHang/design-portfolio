762435b27afb191bec7ca2aa371ec5a14bf18cba
Lucas Hang <lucas.hhang@gmail.com>
2022-02-12 12:52:57 -0300

    fix: layout styles

--- components/layout/layout.tsx
+++ components/layout/layout.tsx
@@ -1,7 +1,7 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import styles from './layout.module.css';
-import utilStyles from '../styles/utils.module.css';
+import utilStyles from '../../styles/utils.module.css';
 import Link from 'next/link';
 
 const name = 'Your Name';
@@ -12,7 +12,7 @@ export default function Layout({ children, home }) {
         <div className={styles.container}>
             <Head>
                 <link rel="icon" href="/favicon.ico" />
-                <meta name="description" content="Learn how to build a personal website using Next.js" />
+                <meta name="description" content="Portifólio de artes de design gráfico" />
                 <meta
                     property="og:image"
                     content={`https://og-image.vercel.app/${encodeURI(
